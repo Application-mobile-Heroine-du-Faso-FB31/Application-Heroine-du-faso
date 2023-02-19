178135ff930bcf82adaedf0e6931709bd7308a4f
JayTech <71102646+jeremie1207@users.noreply.github.com>
2023-02-19 00:30:06 -0500

    profile activity

--- Heroinedufaso/app/src/main/java/com/example/heroinedufaso/HomePageUser.java
+++ Heroinedufaso/app/src/main/java/com/example/heroinedufaso/HomePageUser.java
@@ -4,17 +4,32 @@ import static android.content.ContentValues.TAG;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class HomePageUser extends AppCompatActivity {
 
+    private Button profileBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page_user);
+
+        profileBtn = (Button) findViewById(R.id.profile_usr_home_btn);
+
+        profileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(HomePageUser.this, ProfileUserActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
