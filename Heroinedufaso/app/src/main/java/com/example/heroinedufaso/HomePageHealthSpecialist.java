eb76de107680de6fc76b59cd14762a0bc7978661
farse028 <farse028@uottawa.ca>
2023-03-25 16:21:55 -0400

    added credits to code used for date picker

--- Heroinedufaso/app/src/main/java/com/example/heroinedufaso/HomePageHealthSpecialist.java
+++ Heroinedufaso/app/src/main/java/com/example/heroinedufaso/HomePageHealthSpecialist.java
@@ -2,13 +2,45 @@ package com.example.heroinedufaso;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HomePageHealthSpecialist extends AppCompatActivity {
+
+    private Button profileBtn;
+    private Button signOutBtn;
+
+    private Button chatConsultationBtn;
+
+    private Button periodEngineBtn;
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page_health_specialist);
+
+        profileBtn = (Button) findViewById(R.id.profile_health_specialist_home_btn);
+        signOutBtn = (Button) findViewById(R.id.sign_out_btn);
+        chatConsultationBtn = (Button) findViewById(R.id.chat_specialist_consultation);
+        //periodEngineBtn = (Button) findViewById(R.id.menstruel);
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+
+
+        chatConsultationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomePageHealthSpecialist.this, ConsultationSpecialist.class));
+            }
+        });
     }
 }
